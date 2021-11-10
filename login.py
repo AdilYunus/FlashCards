bbba0faf83d64e6870b2c46df9ca77e5b89d846d
AdilYunus <91338491+AdilYunus@users.noreply.github.com>
2021-11-10 04:33:43 +0100

    update

--- login.py
+++ login.py
@@ -1,42 +1,45 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 import sys
 import os
 import json
 import menu
 
+
 class Login(QtWidgets.QDialog):
-    
-    user_name=""
+
+    user_name = ""
+
     def __init__(self):
         super(Login, self).__init__()
         uic.loadUi('ui/login.ui', self)
         self.pushButton.clicked.connect(self.login)
-        self.pushButton.setAutoDefault(True)#enter tusuyla da sonraki ikrana gidebilir -A
-        self.pushButton_2.clicked.connect(self.cancel)# login page ->cancel button -A
+        # enter tusuyla da sonraki ikrana gidebilir -A
+        self.pushButton.setAutoDefault(True)
+        # login page ->cancel button -A
+        self.pushButton_2.clicked.connect(self.cancel)
+        self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
         self.show()
-        
+
     def login(self):
-    #login methode
-        self.user_name=self.lineEdit_username.text()
+        # login methode
+        self.user_name = self.lineEdit_username.text()
         if not os.path.isfile('user/'+self.user_name+'.json'):
             self.signup()
-        
         self.cams = menu.Menu(self.user_name)
         self.cams.show()
         self.close()
 
-
     def signup(self):
-    #sign up methode
+        # sign up methode
         self.user_info = {
-            'username':self.user_name,
-            'level':1
-            }
-        
-        js_file_name = 'user/'+self.user_name+'.json'       
-        with open(js_file_name,'w') as jsFile:
-            jsFile.write(json.dumps(self.user_info,indent=4))
-        
-    def cancel(self):#quit
-        self.close()
+            'username': self.user_name,
+            'level': 1,
+            'time': 0
+        }
 
+        js_file_name = 'user/'+self.user_name+'.json'
+        with open(js_file_name, 'w') as jsFile:
+            jsFile.write(json.dumps(self.user_info, indent=4))
+
+    def cancel(self):  # quit
+        self.close()
